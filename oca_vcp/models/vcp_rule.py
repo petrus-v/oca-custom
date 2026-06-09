3bbc3592124b932466a218a1ed2972ce49dea09d
Sébastien BEAU <sebastien.beau@akretion.com>
2026-06-09 18:42:59 +0200

    oca_vcp: fix url path of image in fragment

--- oca_vcp/models/vcp_rule.py
+++ oca_vcp/models/vcp_rule.py
@@ -5,6 +5,7 @@
 
 import logging
 import os
+import re
 from pathlib import Path
 
 import pypandoc
@@ -27,6 +28,13 @@ class VcpRule(models.Model):
             repository_branch, module_id, manifest_path
         )
         module_path = os.path.dirname(manifest_path)
+        repo = repository_branch.repository_id
+        module = self.env["vcp.odoo.module"].browse(module_id)
+        base_raw_url = (
+            f"https://raw.githubusercontent.com/{repo.platform_id.name}/{repo.name}"
+            f"/{repository_branch.branch_id.name}/{module.name}"
+        )
+
         readme_path = Path(module_path, "readme")
         vals["readme_fragments"] = {}
         if readme_path.exists():
@@ -38,9 +46,9 @@ class VcpRule(models.Model):
                     if not data:
                         continue
                     if extension == ".md":
-                        vals["readme_fragments"][filename] = data
+                        fragment = data
                     elif extension == ".rst":
-                        vals["readme_fragments"][filename] = pypandoc.convert_text(
+                        fragment = pypandoc.convert_text(
                             data,
                             format="rst",
                             to=PANDOC_MARKDOWN_FORMAT,
@@ -49,14 +57,20 @@ class VcpRule(models.Model):
                         )
                     else:
                         _logger.error("Unsupported format in readme path %s".format())
+                        continue
+                    vals["readme_fragments"][filename] = re.sub(
+                        r"(\!\[.*?\]\()\.\./static/description/",
+                        rf"\1{base_raw_url}/static/description/",
+                        fragment,
+                    )
 
-            if Path(module_path, "static/description/icon.png").exists():
-                module = self.env["vcp.odoo.module"].browse(module_id)
-                repo_name = repository_branch.repository_id.name
-                orga_name = repository_branch.repository_id.platform_id.name
-                vals["icon_url"] = (
-                    f"https://raw.githubusercontent.com/{orga_name}/{repo_name}"
-                    f"/refs/heads/{repository_branch.branch_id.name}/"
-                    f"{module.name}/static/description/icon.png"
-                )
+        if Path(module_path, "static/description/icon.png").exists():
+            module = self.env["vcp.odoo.module"].browse(module_id)
+            repo_name = repository_branch.repository_id.name
+            orga_name = repository_branch.repository_id.platform_id.name
+            vals["icon_url"] = (
+                f"https://raw.githubusercontent.com/{orga_name}/{repo_name}"
+                f"/refs/heads/{repository_branch.branch_id.name}/"
+                f"{module.name}/static/description/icon.png"
+            )
         return vals
